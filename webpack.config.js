a8fa665d0b9511765c39f1987a33db31d785caa0
Spencer Markowski <spencer@theablefew.com>
2022-07-21 11:41:23 -0400

    Add Forest Scene

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    pureevil: './lib/com/pureevil/Main.js',
+    treeshader: './lib/examples/shaders/TreeShader.js',
+  },
+  devtool: 'eval',
+  devServer: {
+   static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'PureEvilGames',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  }
+};
